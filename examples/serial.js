08bdc6207d1e903a5dc411b063864fafa5cd551a
Ben West <bewest@gmail.com>
2014-12-09 03:06:09 -0800

    stub out and test more stuff
    Control flow
    ------------
    
    Take careful note of calling saw.next( ).
    This seems to fix several obscure bugs.
    Notably, it allows exporting data via a stream, although the interface
    might be a little clumsy.

--- examples/serial.js
+++ examples/serial.js
@@ -3,8 +3,9 @@ var scan = require('../utils/scan');
 var messages = require('../lib/messages');
 var machine = require('../lib/');
 var binary = require('binary');
-var SegfaultHandler = require('segfault-handler');
-SegfaultHandler.registerHandler( );
+var es = require('event-stream');
+// var SegfaultHandler = require('segfault-handler');
+// SegfaultHandler.registerHandler( );
 
 if (!module.parent) {
   scan(function (err, list) {
@@ -39,24 +40,63 @@ if (!module.parent) {
         })
         */
         .ping(console.log.bind(console, 'PING'))
+        .tap(function ( ) {
+          console.log('can count to 5');
+          var cur = 0;
+          this.loop(function (end) {
+            this.ping(console.log.bind(console, 'PING', cur)).sleep(500).tap(function ( ) {
+              console.log('can count', cur);
+              cur++;
+            });
+            if (cur > 5) {
+              end( );
+            }
+
+          });
+        })
         .readFirmwareSettings(console.log.bind(console, 'readFirmwareSettings'))
         .readFirmwareHeader(console.log.bind(console, 'readFirmwareHeader'))
+        /*
         .ReadDatabasePartitions(console.log.bind(console, 'ReadDatabasePartitions'))
         .ReadDatabasePageRange({type: 4 }, console.log.bind(console, 'PageRange'))
-        /*
         .getSensorPageRange(console.log.bind(console, 'Sensor pages'))
         .getReceiverLogPageRange(console.log.bind(console, 'Receiver Log pages'))
         */
         .getEGVPageRange(function with_pages (err, range) {
-          this.readEGVPage(range.start, console.log.bind(console, 'EGV HISTORY'))
+          console.log("READING PAGES via stream");
+          // var cur = range.end;
+          var cur = range.start + 10;
+          var start = cur;
+          /*
+          this.loop(function (end) {
+            this
+              .readEGVRecord(cur, function (err, page) {
+                console.log('PAGE', cur, start-cur, page);
+              })
+              .tap(function ( ) {
+                console.log("CURRENT", cur, cur < range.start, range);
+                cur--;
+                if (cur < range.start) end( );
+              }) ;
+          });
+          */
+          tx.createRangedEGVStream(this, range.start, range.start + 17)
+            .pipe(es.writeArray(function (err, results) {
+              console.log("PAGED STREAM RESULTS", err, results);
+            }));
+
         })
+        /*
         .readManufacturingData(0, console.log.bind(console, 'MANUFACTURING DATA 111'))
         .readFirmwareParameterData(0, console.log.bind(console, 'FirmwareParameterData'))
+        */
         .readPCParameterData(0, console.log.bind(console, 'PCParameterData'))
+        /*
         .readSensorDataPageRange(function with_pages (err, range) {
           console.log('SENSOR PAGE RANGE', range.start, range.end);
           this.readSensorData(range.start, console.log.bind(console, 'SENSOR PAGE'))
         })
+        */
         .close( );
         ;
       })
